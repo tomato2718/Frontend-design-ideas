e14eb0abb06749c9b6008e87e01a53ec51ee7d04
tomato2718 <qweasdzxc030@gmail.com>
2024-08-06 20:39:11 +0800

    Chore: Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    assetPrefix: "/Frontend-design-ideas"
-};
+const nextConfig = process.env.DEV_MODE
+  ? {}
+  : {
+      output: "export",
+      basePath: "/Frontend-design-ideas",
+    };
 
 export default nextConfig;
