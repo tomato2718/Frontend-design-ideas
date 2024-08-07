a71973929925b1cc80f16333353acb0a5ef6c929
tomato2718 <55865760+tomato2718@users.noreply.github.com>
2024-08-08 00:33:51 +0800

    Revert "Feat: create home page" (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div className="flex flex-col m-2 gap-2">
-      <h1 className="text-2xl">HomePage</h1>
-      <span>yeah, nothing for now :P</span>
-      <h2 className="text-xl">Pages</h2>
-      <ul className="list-inside list-disc">
-        <li className="text-light-blue-11 hover:text-light-blue-12">
-          <Link href="/theme-toggles">theme toggles</Link>
-        </li>
-      </ul>
-    </div>
+      <>123</>
   );
 }
