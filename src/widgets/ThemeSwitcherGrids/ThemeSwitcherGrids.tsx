98630e4aeb8817a9503e86a45c1f53ae354fb2dd
tomato2718 <qweasdzxc030@gmail.com>
2024-08-08 20:14:04 +0800

    Feat: EclipseToggle showcase

--- src/widgets/ThemeSwitcherGrids/ThemeSwitcherGrids.tsx
+++ src/widgets/ThemeSwitcherGrids/ThemeSwitcherGrids.tsx
@@ -1,32 +1,18 @@
-import clsx from "clsx";
+import { EclipseToggleImp } from "./switchers";
 
 export function ThemeSwitcherGrids() {
   return (
     <div className="h-full grid grid-cols-8 grid-rows-6">
-      <GridContainer componentName="example">
-        <>123</>
+      <GridContainer>
+        <EclipseToggleImp />
       </GridContainer>
     </div>
   );
 }
 
-function GridContainer({
-  componentName,
-  children,
-}: {
-  componentName: string;
-  children: React.ReactElement;
-}) {
+export function GridContainer({ children }: { children: React.ReactElement }) {
   return (
-    <div
-      className={clsx(
-        "flex items-center justify-center",
-        "col-span-1 row-span-1",
-        "border border-[#000000]",
-        "relative",
-      )}
-    >
-      <span className="absolute top-2 left-2">{componentName}</span>
+    <div className="col-span-1 row-span-1 border border-[#000000]">
       {children}
     </div>
   );
