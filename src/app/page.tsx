b5dc436cf9534c65d8413cace93047eb0fcdd2f3
tomato2718 <55865760+tomato2718@users.noreply.github.com>
2024-08-12 17:36:52 +0800

    Feat: Create CardLayout (#5)
    * Feat: Update Homepage
    
    * Feat: create card layout showcase page
    
    * Feat: Create CardLayout
    
    * Feat: CardLayoutShowcase prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,19 @@ export default function Home() {
   return (
     <div className="flex flex-col m-2 gap-2">
       <h1 className="text-2xl">HomePage</h1>
-      <span>yeah, nothing for now :P</span>
+      <span>yeah, haven&#39;t create hompage yet :P</span>
       <h2 className="text-xl">Pages</h2>
       <ul className="list-inside list-disc">
         <li className="text-accent-11 hover:text-accent-12">
           <Link href="/theme-switcher">theme switcher</Link>
+        </li>
+        <li>
+          <span>layout</span>
+          <ul className="list-inside list-disc ps-4">
+            <li className="text-accent-11 hover:text-accent-12">
+              <Link href="/layout/card-layout">card layout</Link>
+            </li>
+          </ul>
         </li>
       </ul>
     </div>
