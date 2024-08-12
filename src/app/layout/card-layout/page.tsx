77d25d57c5ddbe2eeef8de55c6aab44e261339db
tomato2718 <qweasdzxc030@gmail.com>
2024-08-12 16:48:14 +0800

    Feat: create card layout showcase page

---
+++ src/app/layout/card-layout/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { CardLayoutShowcase } from "@/widgets/LayoutShowcase/CardLayoutShowcase";
+
+export default function Page() {
+  return (
+    <>
+      <title>Card Layout</title>
+      <CardLayoutShowcase />
+    </>
+  );
+}
